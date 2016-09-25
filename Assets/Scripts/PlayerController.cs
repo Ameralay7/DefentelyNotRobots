112e460990b1bea5d1ad0e2af1ff19079b58c22a
Amera Ahmed <ameralay7@gmail.com>
2016-09-26 00:30:05 +0200

    working on controllers

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,174 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Transform target;
+    private Animator anim;
+    private Rigidbody rigidBody;
+    private Transform Body;
+
+    public float forwardSpeed = 3;
+    public float forwardDistanceMove = 10;
+    public float dashDelay = .5f;
+    public float stopMoveSmooth = 2;
+    public float backwardSpeed = 2;
+    public float backwardDistanceMove = 12;
+    public float rightSpeed = 3;
+    public float rightDistanceMove = 6;
+    public float rightMoveAngle = 30;
+
+    public Vector3 Distination;
+
+    public bool dashForward;
+    public bool dashBackward;
+    public bool dashRight;
+    public bool dashLeft;
+
+    public bool isMovingBack;
+
+    public float speed;
+	void Start () {
+        anim = GetComponentInChildren<Animator>();
+        rigidBody = GetComponent<Rigidbody>();
+        Body = transform.FindChild("playerModel");
+        target = GameObject.FindGameObjectWithTag("target").transform;
+	}
+	
+	void FixedUpdate () {
+        if (dashForward)
+        {
+            rigidBody.AddForce(transform.forward * speed);
+            //transform.Translate(transform.forward * Time.deltaTime * forwardSpeed, Space.Self);
+            if (Vector3.Distance(transform.position, Distination) < 1f)
+            {
+                speed = 0;
+                //rigidBody.velocity = Vector3.zero;
+               
+                EndDash();
+            }
+        }
+
+        if (dashBackward)
+        {
+            rigidBody.AddForce(transform.forward * speed);
+            //transform.Translate(transform.forward * Time.deltaTime * forwardSpeed, Space.Self);
+            if (Vector3.Distance(transform.position, Distination) < 1f)
+            {
+                speed = 0;
+                //rigidBody.velocity = Vector3.zero;
+
+                EndRollBack();
+            }
+        }
+
+        if (dashRight)
+        {
+            Vector3 direction = Distination - transform.position;
+            rigidBody.AddForce(direction * speed);
+            //transform.Translate(transform.forward * Time.deltaTime * forwardSpeed, Space.Self);
+            if (Vector3.Distance(transform.position, Distination) < 1f)
+            {
+                speed = 0;
+                //rigidBody.velocity = Vector3.zero;
+
+                EndDash();
+            }
+        }
+
+
+        if (speed == 0)
+        {
+            rigidBody.velocity = Vector3.Lerp( rigidBody.velocity, Vector3.zero, Time.deltaTime * stopMoveSmooth);
+        }
+	}
+
+    public void StepForward()
+    {
+        anim.SetTrigger("startDash");
+        Invoke("StartDelayedDashFwd", dashDelay);
+        
+    }
+
+    void StartDelayedDashFwd()
+    {
+        speed = forwardSpeed;
+        dashForward = true;
+        Distination = transform.position + new Vector3(0, 0, forwardDistanceMove);
+    }
+
+    private void EndDash()
+    {
+        anim.SetTrigger("endDash");
+        dashForward = false;
+        dashRight = false;
+
+        transform.LookAt(target);
+        Body.LookAt(target);
+    }
+
+    public void StepBackward()
+    {
+        speed = -backwardSpeed;
+        anim.SetBool("rollBack", true);
+        dashBackward = true;
+        isMovingBack = true;
+        Distination = transform.position - new Vector3(0, 0, backwardDistanceMove);
+
+        //Invoke("HoldRollBack", 1.5f);
+        CancelInvoke("DelayBackwardForCamera");
+    }
+
+   
+
+    private void EndRollBack()
+    {
+        anim.SetBool("rollBack", false);
+        dashBackward = false;
+        if (IsInvoking("DelayBackwardForCamera"))
+            CancelInvoke("DelayBackwardForCamera");
+        Invoke("DelayBackwardForCamera", 1);
+    }
+    void DelayBackwardForCamera()
+    {
+        isMovingBack = false;
+    }
+
+    public void StepRight(){
+
+        float playerTargetDistance = Vector3.Distance(target.position, transform.position);
+        float zValue = (Mathf.Sin(rightMoveAngle * Mathf.Deg2Rad / 2)) * (Mathf.Sqrt(2 * Mathf.Pow(playerTargetDistance, 2) - 2 * Mathf.Pow(playerTargetDistance, 2)*(Mathf.Cos(rightMoveAngle))));
+        // zValue = (sin rightMoveAngle/2 ) * k
+        // k sq = 2 L sq - 2 L sq * cos rightMoveAngle;
+
+        Distination = transform.position + new Vector3(rightDistanceMove, 0, zValue);
+
+        //Vector3 radius = new Vector3(Vector3.Distance(target.position, transform.position)/2, 0, rightDistanceMove);
+        //Quaternion rotation = transform.rotation;
+        //rotation.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+
+        //Distination = rotation * radius;
+       // Distination.y = transform.position.y;
+
+        //Distination = transform.position + new Vector3(rightDistanceMove, 0, 0);
+
+
+        Vector3 direction = Distination - transform.position;
+
+        Body.forward = direction;
+        anim.SetTrigger("startDash");
+        Invoke("StartDelayedDashRight", dashDelay);
+
+    }
+
+    void StartDelayedDashRight()
+    {
+        speed = rightSpeed;
+        dashRight = true;
+
+        
+    }
+
+
+    
+}
--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -13,6 +13,7 @@ public class ThirdPersonCamera : MonoBehaviour {
     public float distanceRight;
     public float smooth;
     public float flipSmooth;
+    public float rotateSmooth;
 
     public float Yoffset;
 
@@ -41,10 +42,25 @@ public class ThirdPersonCamera : MonoBehaviour {
             
             //////
             Vector3 radius = new Vector3(distanceRight, Yoffset,0);
-            Quaternion rotation = Quaternion.identity;
+            //Quaternion rotation = Quaternion.identity;
+            Quaternion rotation = follow.rotation;
             rotation.eulerAngles = new Vector3(0, follow.eulerAngles.y, 0);
+           // radius.y = 0;
+            //if(!follow.parent.GetComponent<PlayerController>().dashForward)
 
-            follow.position = Vector3.Lerp(follow.position, rotation * radius, Time.deltaTime * smooth);
+            //if (follow.parent.GetComponent<PlayerController>().dashBackward)
+            //{
+            //    if (Vector3.Distance(follow.position, target.position) > 3)
+            //        follow.localPosition = Vector3.Lerp(follow.localPosition, rotation * radius, 500);
+            //}
+            //else
+            //{
+                if (Vector3.Distance(follow.position, target.position) > 3)
+                    follow.localPosition = Vector3.Slerp(follow.localPosition, rotation * radius, Time.deltaTime * smooth);
+           // }
+
+            
+
 
            
             ///////
@@ -52,21 +68,22 @@ public class ThirdPersonCamera : MonoBehaviour {
             Vector3 PlayerForward = follow.parent.forward.normalized;
             Vector3 PlayerToTargetDir = target.parent.position - follow.parent.position;
 
-            Vector3 PlayerToFollowDirection = follow.position - follow.parent.position;
+            //Vector3 PlayerToFollowDirection = follow.position - follow.parent.position;
 
 
 
-            Vector3 temp1 = transform.forward;
-            Vector3 temp2 = follow.forward;
+            //Vector3 temp1 = transform.forward;
+            //Vector3 temp2 = follow.forward;
             //test
             //Vector3.OrthoNormalize(ref temp1, ref temp2);
 
-            transform.forward = temp1;
-            follow.forward = temp2;
+            //transform.forward = temp1;
+            //follow.forward = temp2;
 
+            float angle = Vector3.Angle(PlayerForward, PlayerToTargetDir);
+            Debug.Log(angle);
 
-
-            if (GetDirectionSign(PlayerForward, PlayerToTargetDir) > 0)
+            if (GetDirectionSign(PlayerForward, PlayerToTargetDir) > 0  && angle>15)
             {//other player on the right
 
                 distanceRight *= (distanceRight > 0) ? 1 : -1;
@@ -75,29 +92,34 @@ public class ThirdPersonCamera : MonoBehaviour {
 
                 temp.x *= (temp.x >= 0) ? 1 : -1;
 
-                follow.position = Vector3.Lerp(follow.position, temp, Time.deltaTime * flipSmooth);
+                follow.position = Vector3.Slerp(follow.position, temp, Time.deltaTime * flipSmooth);
 
                 //follow.position = temp;
             }
-            else if (GetDirectionSign(PlayerForward, PlayerToTargetDir) < 0)
+            else if (GetDirectionSign(PlayerForward, PlayerToTargetDir) < 0 && angle < 15)
             {//other on the left
                 distanceRight *= (distanceRight > 0) ? -1 : 1;
                 Vector3 temp = follow.position;
 
                 temp.x *= (temp.x >= 0) ? -1 : 1;
 
-                follow.position = Vector3.Lerp(follow.position, temp, Time.deltaTime * flipSmooth);
+                follow.position = Vector3.Slerp(follow.position, temp, Time.deltaTime * flipSmooth);
 
                 //follow.position = temp;
             }
 
+            if (follow.parent.GetComponent<PlayerController>().isMovingBack){
+                
+                transform.position = targetPosition;
+            }
+               
+            else
+                transform.position = Vector3.Slerp(transform.position, targetPosition, Time.deltaTime * smooth);
 
-            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
 
 
-
-
-            transform.LookAt(follow);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(follow.position - transform.position), Time.deltaTime * rotateSmooth);
+            //transform.LookAt(follow);
         }
 	}
 
